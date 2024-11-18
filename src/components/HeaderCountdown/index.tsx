e82d654fbc24bad3d983491eac51df4b6b5c9772
Rodrigo Gomez <rodrigogomez@Rodrigos-MacBook-Pro.local>
2024-11-18 00:42:49 -0300

    Clean up

--- src/components/HeaderCountdown/index.tsx
+++ src/components/HeaderCountdown/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Countdown from "../Countdown";
 import * as styles from "./index.module.scss";
-
+import Logo from "../../assets/Logo";
 const HeaderComponent = () => {
 	return (
 		<div className={`${styles.container}`}>
 			<header className={`${styles.header}`}>
-				<div className={`${styles.left}`}>FirstLeaf</div>
+				<div className={`${styles.left}`}>
+					<Logo height={"2rem"} />
+				</div>
 				<div className={`${styles.right}`}>
 					<Countdown customClass={styles.customCountdown} />
 					<button type="button" className={`${styles.button}`}>
